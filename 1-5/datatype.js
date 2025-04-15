de0090d75fe185644e6669b2af9caeaa5bf9c9a2
Ayushsingh7434 <ayushbhu1304@gmail.com>
2025-04-15 15:35:35 +0000

    done

---
+++ 1-5/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//  alert(3+3)  // we are using nodejs not browser thatswhy we can't use alert here
+
+console.log("Ayush")
+console.log(3+3)
+
+// datatypes
+let n=123 // number datatype
+/* range = 2^53  
+if any no is beyond this range use bigint */
+
+let s="ayush" // sting datatype
+
+let ispresent = true //boolean datatype
+
+let count = null //null datatype : its a standalone value
+
+let c // undefined datatype
+
+console.log(typeof 3.14); //number
+console.log(typeof count); //undefines
+console.log(typeof c); // object : null is a type of object datatype
+console.log(s);
---
+++ 6-10/datatype-coversion.js
@@ -0,0 +1,37 @@
+let s = "324"
+
+console.log(typeof s)
+
+let value = Number(s) // string type converted to number
+
+console.log(typeof value)
+console.log( value)
+
+let s2="33abd"
+
+let val =Number(s2) // string type is cnverted into number but the value is not number type
+
+console.log(typeof val)
+console.log( val)
+
+let s3=null
+
+let val2 =Number(s3) // string type is cnverted into number but the value is not number type
+
+console.log(typeof val2)
+console.log( val2)
+
+ // "33" = 33
+ // "33abs" = NaN
+// null = 0;
+// undefined = NaN
+// true = 1
+// false = 0
+
+// in conversion to datatype into boolean
+// any no except 0 is ture
+// 0 = false
+// empty string "" = false
+// "sukad" = true
+ 
+
