092631938ac0cfa120df2e506164b429968e13fa
Ayushsingh7434 <ayushbhu1304@gmail.com>
2025-04-15 16:09:42 +0000

    comparison

---
+++ 6-10/comparison-lec_8.js
@@ -0,0 +1,19 @@
+// copmarisons and its operators
+
+console.log("32" >1)
+console.log("32" < 1)
+
+console.log("for null")
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >=0) // these are considered to be bad practice
+
+console.log("for undefined")
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >=0) // these are considered to be bad practice
+
+// ===
+console.log("for ===")
+
+console.log("32"===32)
